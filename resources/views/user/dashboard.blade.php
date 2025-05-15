c87cea559f2a864aa0428b993f34eea9c80e754a
AlwanKemal <alwankemal@student.telkomuniversity.ac.id>
2025-05-15 19:55:54 +0700

    Update

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -6,7 +6,6 @@
 <div class="container mt-5">
     <div class="card p-4 shadow">
         <h3 class="mb-3">Selamat Datang, {{ auth()->user()->name }}!</h3>
-        <h1 class="mb-4">Dashboard</h1>
         <p>Silakan pilih tindakan berikut:</p>
         <a href="{{ route('leave.create') }}" class="btn btn-primary mb-2 text-white">Ajukan Cuti</a>
         <a href="{{ route('leave.status') }}" class="btn btn-secondary text-white">Lihat Status Cuti</a>
